684f22916a4f5e405ab21bd06a6e98394f2d0def
lucianopereira <lucianosousapereira1986@gmail.com>
2019-11-01 23:25:51 -0300

    first commit

---
+++ NetCore3-Vue/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore3_Vue.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
---
+++ NetCore3-Vue/client-app/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div :key="r" v-for="r in this.results">{{ r }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                results: []
+            }
+        },
+        async created() {
+            const r = await fetch('/api/values')
+            this.results = await r.json()
+        }
+}
+</script>
